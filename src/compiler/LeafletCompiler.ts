984d6610d439be184e700dbfe11ea5110fb3f6bf
fabian stocker <fabian.stocker3@hotmail.com>
2024-02-15 22:45:35 +0100

    feat: LeafletCompiler generates markers in codeblock

--- src/compiler/LeafletCompiler.ts
+++ src/compiler/LeafletCompiler.ts
@@ -1,37 +1,87 @@
 import { Notice } from "obsidian";
 import { TCompilerStep } from "./GardenPageCompiler";
+import { getAPI } from "obsidian-dataview";
+import { PublishFile } from "src/publishFile/PublishFile";
 
 export class LeafletCompiler {
 	constructor() {}
 
 	compile: TCompilerStep = (file) => async (text) => {
-		const replacedText = text;
+		// let replacedText = '```leaflet\n';
+		let replacedText = text;
 		const leafletRegex = /```leaflet\s(.+?)```/gms;
 		const matches = text.matchAll(leafletRegex);
-
-		console.log("testFile", file);
 
 		if (!matches) {
 			return text;
 		}
 
 		for (const leafletBlock of matches) {
-			try {
-				console.log("leafletBlock", leafletBlock);
-				const markerTagRegex = /markerTag: (.*)/gms;
+			let compiledLeaflet = "";
 
-				console.log(
-					"markerTag",
-					leafletBlock[0].matchAll(markerTagRegex),
+			try {
+				// const markerTagRegex = /markerTag: (.*)\\n/gm;
+				const segments = leafletBlock[1].split("\n");
+
+				const markerTagContainer = segments.find((x) =>
+					x.startsWith("markerTag: "),
 				);
-				// TODO Collect all notes with leaflet marker tag and generate list of markers with there location
+
+				if (markerTagContainer) {
+					const markerTag = markerTagContainer.split(" ")[1];
+
+					const markerFiles = await this.getFilesWithTag(
+						file,
+						markerTag,
+					);
+
+					compiledLeaflet =
+						leafletBlock[1]
+							.replace(markerTagContainer, "")
+							.trimEnd() + "\nmarker:\n";
+
+					for (const markerFile of markerFiles["values"]) {
+						if (markerFile[1] != null) {
+							compiledLeaflet +=
+								"    - default," +
+								markerFile[1][0] +
+								"," +
+								markerFile[1][1] +
+								",[[" +
+								markerFile[0]["path"].replace(".md", "") +
+								"]]," +
+								markerFile[2] +
+								"\n";
+							// compiledLeaflet += 'marker: default,' + markerFile[1][0] + ',' + markerFile[1][1] + ',' + markerFile[0]['path'] + ',,,\n';
+							// marker: default,7.785888671875,12.82603813559322,Redbrands,test,,
+						}
+					}
+				}
 			} catch (e) {
 				new Notice(
 					"Unable to render leaflet map. Please contact Ceron.",
 				);
 			}
+
+			replacedText = replacedText.replace(
+				leafletBlock[1],
+				compiledLeaflet,
+			);
+
+			// replacedText += leafletBlock[1];
 		}
 
 		return replacedText;
 	};
+
+	getFilesWithTag(file: PublishFile, tag: string) {
+		const dvApi = getAPI();
+
+		return dvApi.tryQuery(
+			"TABLE WITHOUT ID file.link, FM.location, file.name FROM #" +
+				tag +
+				" FLATTEN file.frontmatter as FM",
+			file.getPath(),
+		);
+	}
 }
